5244b3d54f38e124354d8691913959169f3014c7
maykenm7 <maykenm7@gmail.com>
2023-05-29 19:33:32 -0500

    Sección de productos 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Header } from './components/header';
+import { ProductList } from './components/ProductList';
 
 function App() {
 
+  const [allProducts, setAllProducts]=useState([]);
+  const [total, setTotal]=useState(0);
+  const [countProducts, setCountProducts]=useState(0);
+
   return (
     <>
-      <h1>Hola</h1>
+      <Header 
+        allProducts={allProducts} 
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList 
+        allProducts={allProducts} 
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
