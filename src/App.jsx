0c79fe0af66825fcd6ea26d29f3c2b709e04a062
SantiagoVillamilC <santyago4993@gmail.com>
2023-05-29 19:31:33 -0500

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './assets/components/home/home'
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <Home/>
     </>
   )
 }
